d9bad3b31c9cc56ee7c4d59d3261f1eaa23248cd
Dayana <dayana82006@gmail.com>
2024-12-19 14:26:46 -0500

    feat: :art: New page aboutme

--- js/app.js
+++ js/app.js
@@ -7,37 +7,75 @@ AOS.init({
   //objeto de texto a traducir 
   const translations = {
     en: {
-        textmenu: "Explore more",
+        textmenu: "Explore More",
         homemenu: "Home",
-        about: "About me",
+        about: "About Me",
         projects: "Projects",
-        contact: "Contact me",
-        titleinfo: "FULL STACK DEVELOPER JUNIOR",
-        shortd: "\"Mistakes are the path to victory\"",
-        description1: "I am a young woman in training with a solid foundation in both technology as in dance, which allows me to integrate creativity and technical skills in multidisciplinary projects.",
+        contact: "Contact Me",
+        titleinfo: "JUNIOR FULL STACK DEVELOPER",
+        shortd: "\"Every mistake is a firm step toward victory.\"",
+        description1: "I am a web developer with experience in HTML, CSS, JavaScript, and Bootstrap. Additionally, I am an artist passionate about contemporary dance, seeking to merge art and technology to create innovative projects.",
         checkcv: "Check my CV",
         download: "Download",
         profilep: "Professional Profile",
-        description2: "Currently, I am developing my knowledge in programming, with a focus on creating innovative digital solutions. My ability to learn quickly allows me to adapt to new tools and languages, facing technological challenges with agility and commitment.",
-        profilec : "Creative Profile",
-        description3 : "At the same time, my dance training has taught me to be disciplined, creative, and to work as a team, skills that I also apply in the technological field. My interest lies in being able to merge these two worlds, exploring how technology can enhance artistic expression and vice versa. I am looking to develop myself in fields such as web development and process automation, always with an approach that values creativity, innovation, and collaborative work.",
-      },
+        description2: "I am currently developing my knowledge in programming, focusing on creating innovative digital solutions. My ability to learn quickly allows me to adapt to new tools and languages, tackling technological challenges with agility and commitment.",
+        profilec: "Creative Profile",
+        description3: "At the same time, my training in dance has taught me to be disciplined, creative, and a team player—skills that I also apply in the technological field. My interest lies in merging these two worlds, exploring how technology can enhance artistic expression and vice versa. I aim to grow in areas such as web development and process automation, always with a focus on creativity, innovation, and collaborative work.",
+        seemore: "See More",
+    },
     es: {
-        textmenu: "Explorar más",
+        textmenu: "Explorar Más",
         homemenu: "Inicio",
-        about: "Acerca de mí",
+        about: "Acerca De Mí",
         projects: "Proyectos",
         contact: "Contáctame",
         titleinfo: "DESARROLLADORA JUNIOR FULL STACK",
-        shortd: "\"Los errores son el camino a la victoria\"",
-        description1: "Soy una joven en formación con una sólida base tanto en tecnología como en danza, lo que me permite integrar creatividad y habilidades técnicas en proyectos multidisciplinarios.",
+        shortd: "\"Cada error es un paso firme hacia el triunfo.\"",
+        description1: " Soy desarrolladora web con experiencia en HTML, CSS, JavaScript y Bootstrap. Además, soy artista apasionada por la danza contemporánea, buscando fusionar el arte y la tecnología para crear proyectos innovadores. ",
         checkcv: "Revisa mi CV",
         download: "Descargar",
         profilep: "Perfil profesional",
         description2: "Actualmente, estoy desarrollando mis conocimientos en programación, con un enfoque en la creación de soluciones digitales innovadoras. Mi capacidad para aprender rápidamente me permite adaptarme a nuevas herramientas y lenguajes, enfrentando desafíos tecnológicos con agilidad y compromiso.",
         profilec : "Perfil Creativo",
         description3 : "Al mismo tiempo, mi formación en danza me ha enseñado a ser disciplinada, creativa y a trabajar en equipo, habilidades que también aplico en el ámbito tecnológico. Mi interés radica en poder fusionar estos dos mundos, explorando cómo la tecnología puede potenciar la expresión artística y viceversa. Busco desarrollarme en campos como el desarrollo web y la automatización de procesos, siempre con un enfoque que valore la creatividad, la innovación y el trabajo colaborativo.",
-      },
+        seemore : "Ver Más",
+    },
+    it: {
+        textmenu: "Esplora di Più",
+        homemenu: "Home",
+        about: "Chi Sono",
+        projects: "Progetti",
+        contact: "Contattami",
+        titleinfo: "SVILUPPATORE FULL STACK JUNIOR",
+        shortd: "\"Ogni errore è un passo deciso verso la vittoria.\"",
+        description1: "Sono una sviluppatrice web con esperienza in HTML, CSS, JavaScript e Bootstrap. Inoltre, sono un'artista appassionata di danza contemporanea, cercando di unire arte e tecnologia per creare progetti innovativi.",
+        checkcv: "Controlla il mio CV",
+        download: "Scarica",
+        profilep: "Profilo Professionale",
+        description2: "Attualmente sto sviluppando le mie conoscenze di programmazione, con un focus sulla creazione di soluzioni digitali innovative. La mia capacità di apprendere rapidamente mi consente di adattarmi a nuovi strumenti e linguaggi, affrontando le sfide tecnologiche con agilità e impegno.",
+        profilec: "Profilo Creativo",
+        description3: "Allo stesso tempo, la mia formazione nella danza mi ha insegnato a essere disciplinata, creativa e a lavorare in squadra, competenze che applico anche nell'ambito tecnologico. Il mio interesse risiede nell'unire questi due mondi, esplorando come la tecnologia possa potenziare l'espressione artistica e viceversa. Punto a crescere in campi come lo sviluppo web e l'automazione dei processi, sempre con un approccio che valorizzi la creatività, l'innovazione e il lavoro collaborativo.",
+        seemore: "Vedi di Più",
+    },
+    fr: {
+        textmenu: "Explorer Plus",
+        homemenu: "Accueil",
+        about: "À Propos de Moi",
+        projects: "Projets",
+        contact: "Contactez-moi",
+        titleinfo: "DÉVELOPPEUSE FULL STACK JUNIOR",
+        shortd: "\"Chaque erreur est un pas ferme vers la victoire.\"",
+        description1: "Je suis développeuse web avec de l'expérience en HTML, CSS, JavaScript et Bootstrap. En outre, je suis une artiste passionnée par la danse contemporaine, cherchant à fusionner l'art et la technologie pour créer des projets innovants.",
+        checkcv: "Consultez mon CV",
+        download: "Télécharger",
+        profilep: "Profil Professionnel",
+        description2: "Actuellement, je développe mes connaissances en programmation, avec un accent sur la création de solutions numériques innovantes. Ma capacité à apprendre rapidement me permet de m'adapter à de nouveaux outils et langages, en affrontant les défis technologiques avec agilité et engagement.",
+        profilec: "Profil Créatif",
+        description3: "Parallèlement, ma formation en danse m'a appris à être disciplinée, créative et à travailler en équipe, des compétences que j'applique également dans le domaine technologique. Mon intérêt réside dans la fusion de ces deux mondes, en explorant comment la technologie peut enrichir l'expression artistique et vice versa. Je vise à évoluer dans des domaines tels que le développement web et l'automatisation des processus, toujours avec une approche valorisant la créativité, l'innovation et le travail collaboratif.",
+        seemore: "Voir Plus",
+    },
+    
+    
   };
   
   // array de id no traducidos
@@ -74,3 +112,7 @@ AOS.init({
         }
     });
   }
+
+  
+  
+  
