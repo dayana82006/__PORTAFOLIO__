fc5564c152c18f7faaf0453da0bc3968af6a967c
Dayana <dayana82006@gmail.com>
2025-01-06 10:55:20 -0500

    feat: :sparkles: update projects

--- js/app.js
+++ js/app.js
@@ -1,10 +1,4 @@
-AOS.init({
-    duration: 1200, // Duración de la animación en milisegundos
-    easing: 'ease', // Tipo de transición
-    once: true, // Si debe ocurrir solo una vez
-  });
-  
-  //objeto de texto a traducir 
+
   const translations = {
     en: {
         textmenu: "Explore More",
@@ -22,6 +16,38 @@ AOS.init({
         profilec: "Creative Profile",
         description3: "At the same time, my training in dance has taught me to be disciplined, creative, and a team player—skills that I also apply in the technological field. My interest lies in merging these two worlds, exploring how technology can enhance artistic expression and vice versa. I aim to grow in areas such as web development and process automation, always with a focus on creativity, innovation, and collaborative work.",
         seemore: "See More",
+        es: "Spanish",
+        in: "English",
+        it: "Italian",
+        level: "Level",
+        native: "Native",
+        skillsT: "Technical Skills",
+        programming: "Programming",
+        knowledge: "Knowledge in Dance",
+        skillsP: "Personal Skills",
+        adaptation: "Adaptability to Change",
+        resp: "Responsibility",
+        perp: "Perseverance",
+        history: "My journey with technology began in school when I was part of the robotics team. It was there that I discovered my fascination with how the digital world works and the possibility of creating new things through programming. This interest grew even more when I saw my father programming; watching him in action inspired me to learn and follow his example, motivating me to explore this field. However, it was not always a straight path. There was a time when I tried to join the Air Force, but I didn't succeed. Although that failure frustrated me, it was precisely my perseverance and determination that allowed me to overcome that obstacle. Instead of staying stuck, I decided that the most important thing was to move forward, seeking new opportunities to learn and grow. I've always had an insatiable curiosity for learning. Over the years, I've explored various areas like drawing, painting, music, sports, but the most important of them was dance, especially ballet, an art that has fascinated me since I was little. However, it was only this year that I was finally able to start practicing it, which taught me that it is never too late to pursue our dreams and passions, no matter how unattainable they may seem. On my journey as a programmer, I found a campus that has been key in my development, where I have learned not only programming but also soft skills and English, which have been essential for my growth. Programming is a fascinating world, and I love discovering how behind every webpage or game, there is a complex structure that machines interpret through code. This ability to understand and give life to what seems invisible excites me and drives me to keep exploring and creating.",
+        myshorthistory: "My Short History",
+        textcontactme: "Contact Me",
+        myprojects: "My Projects",
+        titleproject: "HamburguerPlus",
+        descriptionproject: "A web page created with a minimalist and functional approach, developed exclusively with pure HTML and CSS",
+        descriptionp1: "Hamburguer Plus is a minimalist and functional website created exclusively with pure HTML and CSS. This project aims to highlight the importance of the fundamentals in web development, showing how an attractive and functional design can be achieved without relying on frameworks or external tools.",
+        tcu: "Technologies Used",
+        dschtml: "It is the markup language that structures and defines the content of a webpage.",
+        dsccss: "CSS is the language responsible for the appearance and design of a webpage.",
+        dscvisual: "It is a text editor designed to program efficiently.",
+        bencr: "Benefits and Features",
+        interfaz: "Intuitive Interface",
+        dscinterfaz: "It works perfectly on desktop devices, ensuring a fast and smooth experience.",
+        opt: "PC Optimization",
+        dscopt: "The site is optimized for desktop devices, ensuring smooth navigation and a design adapted to large screens.",
+        idm: "Languages I Practice",
+        copy: "2025 Dayana Hernández - All rights reserved."
+
+
     },
     es: {
         textmenu: "Explorar Más",
@@ -39,6 +65,37 @@ AOS.init({
         profilec : "Perfil Creativo",
         description3 : "Al mismo tiempo, mi formación en danza me ha enseñado a ser disciplinada, creativa y a trabajar en equipo, habilidades que también aplico en el ámbito tecnológico. Mi interés radica en poder fusionar estos dos mundos, explorando cómo la tecnología puede potenciar la expresión artística y viceversa. Busco desarrollarme en campos como el desarrollo web y la automatización de procesos, siempre con un enfoque que valore la creatividad, la innovación y el trabajo colaborativo.",
         seemore : "Ver Más",
+        idm : "Idiomas que práctico",
+        es : "Español",
+        in : "Ingles",
+        it : "Italiano",
+        level : "Nivel",
+        native : "Nativo",
+        skillsT : "Habilidades Técnicas",
+        programming : "Programacion",
+        knowledge : "Conocimientos en Danza",
+        skillsP : "Habilidades Personales",
+        adaptation : "Adaptación al cambio",
+        resp : "Responsabilidad",
+        perp : "Perseverancia",
+        history : "Mi historia con la tecnología comenzó en el colegio cuando formaba parte del equipo de robótica.  Fue allí donde descubrí mi fascinación por cómo funciona  el mundo digital y la posibilidad de crear cosas nuevas  con la programación. Ese interés creció aún más cuando vi  a mi papá programar; verlo en acción me inspiró a aprender  y seguir su ejemplo, y me dio la motivación para explorar este campo.  Sin embargo, no todo fue un camino recto.  Hubo un momento en que intenté ingresar a la Fuerza  Aérea, pero no lo logré. Aunque ese fracaso me  produjo frustración, fue precisamente mi perseverancia y  determinación lo que me permitió superar ese obstáculo.  En lugar de quedarme estancada, decidí que lo más importante  era seguir adelante, buscando nuevas oportunidades para aprender y crecer. Siempre he tenido una curiosidad insaciable  por el aprendizaje. A lo largo de los años,  he explorado diversas áreas como el dibujo, la pintura, la musica,  el deporte, pero la mas importante de ellas fue la danza,  en especial el ballet, un arte que me llamó la atención  desde pequeña. Sin embargo, fue hasta este año  que finalmente pude empezar a practicarlo, lo  que me enseñó que nunca es tarde para perseguir  nuestros sueños y pasiones, por más que parezcan inalcanzables.  En mi camino como programadora, encontré un campus que ha sido clave en mi formación, donde he aprendido  no solo programación, sino también habilidades blandas e inglés, lo cual ha sido esencial para mi  desarrollo. La programación es un mundo fascinante, y me encanta descubrir cómo detrás de cada página  web o juego, hay una estructura compleja que las máquinas interpretan a través del código. Esta  capacidad de entender y dar vida a lo que parece invisible me llena de emoción, y me impulsa a seguir  explorando y creando.",
+        myshorthistory : "Mi Breve Historia",
+        textcontactme : "Contactame",
+        myprojects: "Mis Proyectos",
+        titleproject: "HamburguerPlus",
+        descriptionproject : "una página web creada con un enfoque minimalista y funcional, desarrollada exclusivamente con HTML y CSS puro",
+        descriptionp1 : "Hamburguer Plus es una página web creada con un enfoque minimalista y funcional, desarrollada exclusivamente con HTML y CSS puro. Este proyecto  busca destacar la importancia de los fundamentos en el desarrollo web, mostrando cómo se puede lograr un diseño atractivo y funcional sin  depender de frameworks o herramientas externas.",
+        tcu : "Tecnologías utilizadas",
+        dschtml : "Es el lenguaje de marcado que estructura y define el contenido de una página web.",
+        dsccss : "CSS es el lenguaje que se encarga de la apariencia y el diseño de una página web.",
+        dscvisual : "Es un editor de texto diseñado para programar de manera eficiente.",
+        bencr : "Beneficios y Características",
+        interfaz : "Interfaz Intuitiva",
+        dscinterfaz : "Funciona perfectamente en dispositivos de escritorio, garantizando una experiencia rápida y fluida.",
+        opt : "Optimización de pc",
+        dscopt : "El sitio está optimizado para dispositivos de escritorio, garantizando una navegación fluida y un diseño adaptado a pantallas grandes.",
+        copy : "2025 Dayana Hernández - Todos los derechos reservados."
+        
     },
     it: {
         textmenu: "Esplora di Più",
@@ -56,7 +113,38 @@ AOS.init({
         profilec: "Profilo Creativo",
         description3: "Allo stesso tempo, la mia formazione nella danza mi ha insegnato a essere disciplinata, creativa e a lavorare in squadra, competenze che applico anche nell'ambito tecnologico. Il mio interesse risiede nell'unire questi due mondi, esplorando come la tecnologia possa potenziare l'espressione artistica e viceversa. Punto a crescere in campi come lo sviluppo web e l'automazione dei processi, sempre con un approccio che valorizzi la creatività, l'innovazione e il lavoro collaborativo.",
         seemore: "Vedi di Più",
-    },
+        idm: "Lingue che pratico",
+        es: "Spagnolo",
+        in: "Inglese",
+        it: "Italiano",
+        level: "Livello",
+        native: "Nativo",
+        skillsT: "Competenze Tecniche",
+        programming: "Programmazione",
+        knowledge: "Conoscenze nella Danza",
+        skillsP: "Competenze Personali",
+        adaptation: "Adattabilità al Cambiamento",
+        resp: "Responsabilità",
+        perp: "Perseveranza",
+        history: "La mia storia con la tecnologia è iniziata a scuola, quando facevo parte del team di robotica. È stato lì che ho scoperto il mio fascino per il funzionamento del mondo digitale e la possibilità di creare nuove cose attraverso la programmazione. Questo interesse è cresciuto ancora di più quando ho visto mio padre programmare; vederlo in azione mi ha ispirato a imparare e seguire il suo esempio, motivandomi a esplorare questo campo. Tuttavia, non è stato sempre un percorso lineare. C'è stato un momento in cui ho provato a entrare nell'Aeronautica Militare, ma non ci sono riuscita. Anche se quel fallimento mi ha frustrato, è stata proprio la mia perseveranza e determinazione che mi hanno permesso di superare quell'ostacolo. Invece di rimanere bloccata, ho deciso che la cosa più importante era andare avanti, cercando nuove opportunità per imparare e crescere. Ho sempre avuto una curiosità insaziabile per l'apprendimento. Nel corso degli anni, ho esplorato vari ambiti come il disegno, la pittura, la musica, lo sport, ma il più importante è stato la danza, in particolare il balletto, un'arte che mi ha affascinato fin da bambina. Tuttavia, è stato solo quest'anno che finalmente ho potuto iniziare a praticarlo, il che mi ha insegnato che non è mai troppo tardi per inseguire i nostri sogni e le nostre passioni, per quanto possano sembrare irraggiungibili. Nel mio percorso come programmatrice, ho trovato un campus che è stato fondamentale per la mia formazione, dove ho imparato non solo la programmazione, ma anche competenze trasversali e inglese, che sono state essenziali per la mia crescita. La programmazione è un mondo affascinante, e mi piace scoprire come dietro ogni pagina web o gioco ci sia una struttura complessa che le macchine interpretano attraverso il codice. Questa capacità di comprendere e dare vita a ciò che sembra invisibile mi entusiasma e mi spinge a continuare a esplorare e creare.",   
+        myshorthistory: "La Mia Breve Storia",
+        textcontactme: "Contattami",
+        myprojects: "I Miei Progetti",
+        titleproject: "HamburguerPlus",
+        descriptionproject: "Una pagina web creata con un approccio minimalista e funzionale, sviluppata esclusivamente con HTML e CSS puro",
+        descriptionp1: "Hamburguer Plus è un sito web minimalista e funzionale creato esclusivamente con HTML e CSS puri. Questo progetto mira a evidenziare l'importanza delle basi nello sviluppo web, mostrando come ottenere un design attraente e funzionale senza fare affidamento su framework o strumenti esterni.",
+        tcu: "Tecnologie Utilizzate",
+        dschtml: "È il linguaggio di markup che struttura e definisce il contenuto di una pagina web.",
+        dsccss: "CSS è il linguaggio responsabile dell'aspetto e del design di una pagina web.",
+        dscvisual: "È un editor di testo progettato per programmare in modo efficiente.",
+        bencr: "Benefici e Caratteristiche",
+        interfaz: "Interfaccia Intuitiva",
+        dscinterfaz: "Funziona perfettamente sui dispositivi desktop, garantendo un'esperienza veloce e fluida.",
+        opt: "Ottimizzazione per PC",
+        dscopt: "Il sito è ottimizzato per dispositivi desktop, garantendo una navigazione fluida e un design adattato a schermi grandi.",
+        copy: "2025 Dayana Hernández - Tutti i diritti riservati."
+        
+        },
     fr: {
         textmenu: "Explorer Plus",
         homemenu: "Accueil",
@@ -73,8 +161,39 @@ AOS.init({
         profilec: "Profil Créatif",
         description3: "Parallèlement, ma formation en danse m'a appris à être disciplinée, créative et à travailler en équipe, des compétences que j'applique également dans le domaine technologique. Mon intérêt réside dans la fusion de ces deux mondes, en explorant comment la technologie peut enrichir l'expression artistique et vice versa. Je vise à évoluer dans des domaines tels que le développement web et l'automatisation des processus, toujours avec une approche valorisant la créativité, l'innovation et le travail collaboratif.",
         seemore: "Voir Plus",
-    },
-    
+        idm: "Langues que je pratique",
+        es: "Espagnol",
+        in: "Anglais",
+        it: "Italien",
+        level: "Niveau",
+        native: "Natif",
+        skillsT: "Compétences Techniques",
+        programming: "Programmation",
+        knowledge: "Connaissances en Danse",
+        skillsP: "Compétences Personnelles",
+        adaptation: "Adaptabilité au Changement",
+        resp: "Responsabilité",
+        perp: "Persévérance",
+        history: "Mon histoire avec la technologie a commencé à l'école lorsque je faisais partie de l'équipe de robotique. C'est là que j'ai découvert ma fascination pour le fonctionnement du monde numérique et la possibilité de créer de nouvelles choses grâce à la programmation. Cet intérêt a encore grandi lorsque j'ai vu mon père programmer ; le voir en action m'a inspiré à apprendre et à suivre son exemple, me motivant à explorer ce domaine. Cependant, le chemin n'a pas toujours été simple. À un moment donné, j'ai essayé d'intégrer l'Armée de l'Air, mais je n'ai pas réussi. Bien que cet échec m'ait frustrée, c'est précisément ma persévérance et ma détermination qui m'ont permis de surmonter cet obstacle. Au lieu de rester bloquée, j'ai décidé que le plus important était d'aller de l'avant, en cherchant de nouvelles opportunités pour apprendre et grandir. J'ai toujours eu une curiosité insatiable pour l'apprentissage. Au fil des ans, j'ai exploré divers domaines comme le dessin, la peinture, la musique, le sport, mais le plus important d'entre eux a été la danse, en particulier le ballet, un art qui m'a attirée depuis mon enfance. Cependant, ce n'est que cette année que j'ai enfin pu commencer à le pratiquer, ce qui m'a appris qu'il n'est jamais trop tard pour poursuivre nos rêves et nos passions, aussi inaccessibles qu'ils puissent paraître. Dans mon parcours en tant que programmeuse, j'ai trouvé un campus qui a été essentiel dans ma formation, où j'ai appris non seulement la programmation, mais aussi des compétences interpersonnelles et l'anglais, qui ont été essentiels à mon développement. La programmation est un monde fascinant, et j'adore découvrir comment, derrière chaque site web ou jeu, se cache une structure complexe que les machines interprètent à travers le code. Cette capacité à comprendre et donner vie à ce qui semble invisible m'excite et me pousse à continuer à explorer et à créer.",
+        myshorthistory: "Mon Histoire Brève",
+        textcontactme: "Contactez-moi",
+        myprojects: "Mes Projets",
+        titleproject: "HamburguerPlus",
+        descriptionproject: "Une page web créée avec une approche minimaliste et fonctionnelle, développée exclusivement avec du HTML et du CSS pur",
+        descriptionp1: "Hamburguer Plus est un site web minimaliste et fonctionnel créé exclusivement avec du HTML et du CSS pur. Ce projet vise à souligner l'importance des bases dans le développement web, en montrant comment concevoir un site attrayant et fonctionnel sans utiliser de frameworks ou d'outils externes.",
+        tcu: "Technologies Utilisées",
+        dschtml: "C'est le langage de balisage qui structure et définit le contenu d'une page web.",
+        dsccss: "CSS est le langage responsable de l'apparence et du design d'une page web.",
+        dscvisual: "C'est un éditeur de texte conçu pour programmer efficacement.",
+        bencr: "Avantages et Caractéristiques",
+        interfaz: "Interface Intuitive",
+        dscinterfaz: "Il fonctionne parfaitement sur les appareils de bureau, garantissant une expérience rapide et fluide.",
+        opt: "Optimisation pour PC",
+        dscopt: "Le site est optimisé pour les appareils de bureau, garantissant une navigation fluide et un design adapté aux grands écrans.",
+        copy: "2025 Dayana Hernández - Tous droits réservés."
+
+        },
+        
     
   };
   
@@ -82,7 +201,10 @@ AOS.init({
   const nonTranslatableIds = [
      'offcanvasDarkNavbar',
       'languageSelector',
-     
+      'projects',
+      'features',
+      'technologies',
+      'footer'
   ];
   
   // cambiar idioma
